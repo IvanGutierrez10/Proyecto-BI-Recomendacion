85713ccdd1b7649b683cfd0adbde0a3a931d2cfb
Ivangutierrez10 <ivangues@unisabana.edu.co>
2024-05-17 16:47:17 -0500

    reajuste del mincof y minsup

--- recommender.py
+++ recommender.py
@@ -83,8 +83,8 @@ class Recommender:
         self.prices = prices
         self.database = database
 
-        minsup = 20  # Example value for minimum support
-        minconf = 0.5  # Example value for minimum confidence
+        minsup = 19  # Example value for minimum support
+        minconf = 0.3  # Example value for minimum confidence
 
         # Find frequent itemsets
         frequent_itemsets = self.eclat(database, minsup)
