c75d8f1f17f69157dec8d817bf126b93a6308779
Hector Diaz <hdiaz35120@gmail.com>
2024-05-17 15:43:06 -0500

    Se agrega ECLAT para el train

--- recommender.py
+++ recommender.py
@@ -6,6 +6,28 @@ class Recommender:
         This is the class to make recommendations.
         The class must not require any mandatory arguments for initialization.
     """
+    def eclat_recursive(self, P, minsup, F):
+        for Xa, t_Xa in P.items():
+            F.append((sorted(list(Xa)), len(t_Xa)))
+            Pa = {}
+            for Xb, t_Xb in P.items():
+                if list(Xb) > list(Xa):
+                    Xab = Xa | Xb
+                    t_Xab = t_Xa & t_Xb
+                    if len(t_Xab) >= minsup:
+                        Pa[Xab] = t_Xab
+            if Pa:
+                self.eclat_recursive(Pa, minsup, F)
+
+    def eclat(self, db, minsup):
+        F = []
+        P = {frozenset([item]): set() for transaction in db for item in transaction}
+        for i, transaction in enumerate(db):
+            for item in transaction:
+                P[frozenset([item])].add(i)
+
+        self.eclat_recursive(P, minsup, F)
+        return F
     
     def train(self, prices, database) -> None:
         """
@@ -14,10 +36,18 @@ class Recommender:
             :param database: a list of lists of item ids that have been purchased together. Every entry corresponds to one transaction
             :return: the object should return itself here (this is actually important!)
         """
-        
-        # do something
-        
-        # return this object again
+        self.prices = prices
+        self.database = database
+
+        minsup = 3
+        minconf = 0.7
+
+        frequent_itemsets = self.eclat(self.database, minsup)
+
+        fsets = [(set(items), sup) for items, sup in frequent_itemsets]
+
+        self.rules = self.getStrongRulesFromFrequentSets(fsets, minconf)
+
         return self
 
     def get_recommendations(self, cart:list, max_recommendations:int) -> list:
